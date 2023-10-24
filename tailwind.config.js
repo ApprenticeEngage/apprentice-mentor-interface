8a24a9b0e6d28e6e60cbb8dfd3dfc9398e683d51
EggsyOnCode <farzansaqib2003@gmail.com>
2023-10-24 19:25:38 +0500

    tailwind css custom colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      "light-bg": "#ECE9F6",
+      "primary": "#98B847",
+      "accent": "#AADF26",
+      "card-color": "#A38C85",
+      "card-dark": "#9C9717",
+      "temp" : "#824057"
+    },
   },
   plugins: [],
 };
