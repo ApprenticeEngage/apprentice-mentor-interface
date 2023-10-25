d60e2dd07d8456f17b9e5e9a33120513305eb0f8
EggsyOnCode <farzansaqib2003@gmail.com>
2023-10-25 21:11:56 +0500

    card tiles finalized

--- src/components/Home/CourseTile.tsx
+++ src/components/Home/CourseTile.tsx
@@ -7,13 +7,15 @@ type CourseTileProps = {
 
 const CourseTile: React.FC<CourseTileProps> = ({ title, status }) => {
   return (
-    <div className="flex items-center bg-card-color w-full p-4 justify-between text-white ">
-      <div className="bg-temp p-3 rounded-l-md"></div>
-      <div className="text-center">
-        <h2 className="text-xl font-bold">{title}</h2>
+    <div className="flex bg-card-dark w-full text-white rounded-lg">
+      <div className="banner rounded-lg bg-temp w-2/12"></div>
+      <div className="text-center p-4 flex justify-between items-center w-full">
+        <div className="flex flex-col items-start">
+        <h2 className="text-3xl font-bold mb-4">{title}</h2>
+        <span className="mr-2 text-sm font-thin">{status}</span>
+        </div>
         <div className="mt-2">
-          <span className="mr-2">{status}</span>
-          <button className="bg-green-500 text-white px-3 py-1 rounded-md">
+          <button className="bg-accent text-white p-2 mb-1 rounded-md">
             Edit/Manage
           </button>
         </div>
