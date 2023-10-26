cea60395b44a0412c22adf9f7e30ca29845a941a
EggsyOnCode <farzansaqib2003@gmail.com>
2023-10-27 00:58:20 +0500

    audience page:Targeted audience done

--- src/components/CourseEdit/Audience.tsx
+++ src/components/CourseEdit/Audience.tsx
@@ -1,21 +1,56 @@
 import React from 'react'
+import { IoPush } from "react-icons/io5";
 
 const Audience = () => {
   return (
-    <div className='w-full flex flex-col justify-start items-start text-text'>
-      <div className='p-12 font-bold'>
-        <h1 className='text-4xl mb-10'>Audience Oriented Insights</h1>
-        <hr className='h-[3px]'/>
+    <div className="w-full flex flex-col justify-start items-start text-text">
+      <div className="p-12 font-bold">
+        <h1 className="text-4xl mb-10">Audience Oriented Insights</h1>
+        <hr className="h-[3px]" />
       </div>
 
+      <h1 className="text-3xl mb-5 px-12 font-semibold">Targeted Audience</h1>
       {/* //targetted audience */}
-      <div className='px-12'>
-
+      <div className="px-12 text-xl font-light flex flex-col mb-12">
+        <p className="mb-3">
+          Tell us what kind of audience your vocational training aims to target?
+          What demographic? What Skillset?
+        </p>
+        <div className="flex flex-row items-center">
+          <input
+            placeholder="Targeted Audience"
+            className="text-accent rounded-lg p-2 grow"
+          />
+          <div className="hover:cursor-pointer">
+            <IoPush size={24} />
+          </div>
+        </div>
       </div>
 
-      
+      <h1 className="text-3xl mb-5 px-12 font-semibold">Targeted Audience</h1>
+      {/* learning outcomes */}
+
+      <div className="px-12 text-xl font-light flex flex-col mb-12">
+        <p className="mb-3">
+          Tell us what kind of audience your vocational training aims to target?
+          What demographic? What Skillset?
+        </p>
+        <div className="flex flex-row items-center">
+          <input
+            placeholder="Targeted Audience"
+            className="text-accent rounded-lg p-2 grow"
+          />
+          <div className="hover:cursor-pointer">
+            <IoPush size={24} />
+          </div>
+        </div>
+      </div>
+
+      {/* pre-req */}
+
+      {/* course difficulty */}
     </div>
-  )
+  );
 }
 
 export default Audience
