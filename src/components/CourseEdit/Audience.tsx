eb1d419086173b8fa2c1cb2253ee01266ab95f01
EggsyOnCode <farzansaqib2003@gmail.com>
2023-10-27 01:00:36 +0500

    audience page:learning otucomes done

--- src/components/CourseEdit/Audience.tsx
+++ src/components/CourseEdit/Audience.tsx
@@ -27,17 +27,44 @@ const Audience = () => {
         </div>
       </div>
 
-      <h1 className="text-3xl mb-5 px-12 font-semibold">Targeted Audience</h1>
+      <h1 className="text-3xl mb-5 px-12 font-semibold">Learning Outcomes</h1>
       {/* learning outcomes */}
 
       <div className="px-12 text-xl font-light flex flex-col mb-12">
         <p className="mb-3">
-          Tell us what kind of audience your vocational training aims to target?
-          What demographic? What Skillset?
+          At the end of the course what will your apprentices have learned? What
+          skills would they have gained? Enlist at least 4!
         </p>
-        <div className="flex flex-row items-center">
+        <div className="flex flex-row items-center mb-3">
           <input
-            placeholder="Targeted Audience"
+            placeholder="Learning Outcome"
+            className="text-accent rounded-lg p-2 grow"
+          />
+          <div className="hover:cursor-pointer">
+            <IoPush size={24} />
+          </div>
+        </div>
+        <div className="flex flex-row items-center mb-3">
+          <input
+            placeholder="Learning Outcome"
+            className="text-accent rounded-lg p-2 grow"
+          />
+          <div className="hover:cursor-pointer">
+            <IoPush size={24} />
+          </div>
+        </div>
+        <div className="flex flex-row items-center mb-3">
+          <input
+            placeholder="Learning Outcome"
+            className="text-accent rounded-lg p-2 grow"
+          />
+          <div className="hover:cursor-pointer">
+            <IoPush size={24} />
+          </div>
+        </div>
+        <div className="flex flex-row items-center mb-3">
+          <input
+            placeholder="Learning Outcome"
             className="text-accent rounded-lg p-2 grow"
           />
           <div className="hover:cursor-pointer">
