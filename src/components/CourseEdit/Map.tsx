aded29057be540ef731d2d52b60c82ea00d09101
EggsyOnCode <farzansaqib2003@gmail.com>
2023-10-27 17:59:13 +0500

    map centers onto our current pos: corrected ; map done

--- src/components/CourseEdit/Map.tsx
+++ src/components/CourseEdit/Map.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@mui/material";
 import { LatLng, latLng, LatLngTuple } from "leaflet";
 import "leaflet/dist/leaflet.css";
 import React, {
@@ -16,12 +17,15 @@ import {
   useMapEvents,
 } from "react-leaflet";
 
+//global var to be stored in DB
+let selectedposition;
 
 function DraggableMarker() {
   const [draggable, setDraggable] = useState(false);
   const [position, setPosition] = useState(null);
   const markerRef = useRef(null);
-
+  //selected pos to be saved
+  const [selectedLocation, setSelectedLocation] = useState(null);
   const [bbox, setBbox] = useState([]);
 
   const map = useMap();
@@ -43,6 +47,7 @@ function DraggableMarker() {
         const marker = markerRef.current;
         if (marker != null) {
           setPosition(marker.getLatLng());
+          selectedposition = marker.getLatLng();
         }
       },
     }),
@@ -52,46 +57,55 @@ function DraggableMarker() {
     setDraggable((d) => !d);
   }, []);
 
-   return position === null ? null : (
-     <Marker
-       draggable={draggable}
-       eventHandlers={eventHandlers}
-       position={position}
-       ref={markerRef}
-     >
-       <Popup minWidth={90}>
-         <Popup>
-           You are here. <br />
-           Map bbox: <br />
-           <b>Southwest lng</b>: {bbox[0]} <br />
-           <b>Southwest lat</b>: {bbox[1]} <br />
-           <b>Northeast lng</b>: {bbox[2]} <br />
-           <b>Northeast lat</b>: {bbox[3]}
-         </Popup>
-         <span onClick={toggleDraggable}>
-           {draggable
-             ? "Marker is draggable"
-             : "Click here to make marker draggable"}
-         </span>
-       </Popup>
-     </Marker>
-   );
+  return position === null ? null : (
+    <Marker
+      draggable={draggable}
+      eventHandlers={eventHandlers}
+      position={position}
+      ref={markerRef}
+    >
+      <Popup minWidth={90}>
+        <span onClick={toggleDraggable}>
+          {draggable
+            ? "Marker is draggable"
+            : "Click here to make marker draggable"}
+        </span>
+      </Popup>
+    </Marker>
+  );
 }
 
 const Map = () => {
+  const [showIcon, setShowIcon] = useState(false);
+
+  const handleButtonClick = () => {
+    console.log(selectedposition);
+    setShowIcon(true);
+  };
+
   return (
-    <MapContainer
-      center={[31.295026, 70.922667]}
-      zoom={13}
-      scrollWheelZoom={true}
-      className="h-[50vh] w-[100vh]"
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <DraggableMarker />
-    </MapContainer>
+    <div className="w-full flex flex-row items-center">
+      <MapContainer
+        center={[31.295026, 70.922667]}
+        zoom={13}
+        scrollWheelZoom={true}
+        className="h-[50vh] w-[100vh]"
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <DraggableMarker />
+      </MapContainer>
+      <div className="flex flex-row justify-start items-center">
+      <button
+        className="bg-card-dark h-fit rounded-lg p-5 ml-5 font-bold"
+        onClick={handleButtonClick}
+      >
+        Confirm position!
+      </button>
+      {showIcon && <FaCheckCircle size={30} color="green" />}
+    </div>
   );
 };
 
