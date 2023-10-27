83ebe9036339176d7f60cd77388c37e45bf2166f
EggsyOnCode <farzansaqib2003@gmail.com>
2023-10-27 12:54:51 +0500

    course edit page:scheduling: schedule table time pickers mui added

--- src/components/CourseEdit/Table.tsx
+++ src/components/CourseEdit/Table.tsx
@@ -1,76 +1,115 @@
 import React from "react";
+import { TimePicker } from "@mui/x-date-pickers";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 const Table = () => {
   return (
     <div className="bg-[#5B5B5B]">
-      <table className="mx-auto">
-        <thead>
-          <tr>
-            <th className="bg-[#5B5B5B] text-white p-2 border border-[#404040]">
-              Weekdays
-            </th>
-            <th className="bg-[#5B5B5B] text-white p-2 border border-[#404040]">
-              Timing Start
-            </th>
-            <th className="bg-[#5B5B5B] text-white p-2 border border-[#404040]">
-              Lesson End
-            </th>
-          </tr>
-        </thead>
-        <tbody className="text-white text-lg">
-          <tr>
-            <td className="bg-[#404040] p-2 border border-[#5B5B5B]">Monday</td>
-            <td className="bg-[#404040] p-2 border border-[#5B5B5B]">
-              8:00 AM
-            </td>
-            <td className="bg-[#404040] p-2 border border-[#5B5B5B]">
-              9:00 AM
-            </td>
-          </tr>
-          <tr>
-            <td className="bg-[#404040] p-2 border border-[#5B5B5B]">
-              Tuesday
-            </td>
-            <td className="bg-[#404040] p-2 border border-[#5B5B5B]">
-              9:00 AM
-            </td>
-            <td className="bg-[#404040] p-2 border border-[#5B5B5B]">
-              10:00 AM
-            </td>
-          </tr>
-          <tr>
-            <td className="bg-[#404040] p-2 border border-[#5B5B5B]">
-              Wednesday
-            </td>
-            <td className="bg-[#404040] p-2 border border-[#5B5B5B]">
-              10:00 AM
-            </td>
-            <td className="bg-[#404040] p-2 border border-[#5B5B5B]">
-              11:00 AM
-            </td>
-          </tr>
-          <tr>
-            <td className="bg-[#404040] p-2 border border-[#5B5B5B]">
-              Thursday
-            </td>
-            <td className="bg-[#404040] p-2 border border-[#5B5B5B]">
-              11:00 AM
-            </td>
-            <td className="bg-[#404040] p-2 border border-[#5B5B5B]">
-              12:00 PM
-            </td>
-          </tr>
-          <tr>
-            <td className="bg-[#404040] p-2 border border-[#5B5B5B]">Friday</td>
-            <td className="bg-[#404040] p-2 border border-[#5B5B5B]">
-              <TimePicker label="Basic time picker" />
-            </td>
-            <td className="bg-[#404040] p-2 border border-[#5B5B5B]">
-              1:00 PM
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <table className="mx-auto">
+          <thead>
+            <tr>
+              <th className="bg-[#5B5B5B] text-white p-2 border border-[#404040]">
+                Weekdays
+              </th>
+              <th className="bg-[#5B5B5B] text-white p-2 border border-[#404040]">
+                Timing Start
+              </th>
+              <th className="bg-[#5B5B5B] text-white p-2 border border-[#404040]">
+                Lesson End
+              </th>
+            </tr>
+          </thead>
+          <tbody className="text-white text-lg">
+            <tr>
+              <td className="bg-[#404040] p-2 border border-[#5B5B5B]">
+                Monday
+              </td>
+              <td className="bg-[#404040] p-2 border border-[#5B5B5B]">
+                <TimePicker
+                  className="bg-[#5B5B5B] rounded-xl"
+                  label="Starting Time"
+                />
+              </td>
+              <td className="bg-[#404040] p-2 border border-[#5B5B5B]">
+                <TimePicker
+                  className="bg-[#5B5B5B] rounded-xl"
+                  label="Ending Time"
+                />
+              </td>
+            </tr>
+            <tr>
+              <td className="bg-[#404040] p-2 border border-[#5B5B5B]">
+                Tuesday
+              </td>
+              <td className="bg-[#404040] p-2 border border-[#5B5B5B]">
+                <TimePicker
+                  className="bg-[#5B5B5B] rounded-xl"
+                  label="Starting Time"
+                />
+              </td>
+              <td className="bg-[#404040] p-2 border border-[#5B5B5B]">
+                <TimePicker
+                  className="bg-[#5B5B5B] rounded-xl"
+                  label="Ending Time"
+                />
+              </td>
+            </tr>
+            <tr>
+              <td className="bg-[#404040] p-2 border border-[#5B5B5B]">
+                Wednesday
+              </td>
+              <td className="bg-[#404040] p-2 border border-[#5B5B5B]">
+                <TimePicker
+                  className="bg-[#5B5B5B] rounded-xl"
+                  label="Starting Time"
+                />
+              </td>
+              <td className="bg-[#404040] p-2 border border-[#5B5B5B]">
+                <TimePicker
+                  className="bg-[#5B5B5B] rounded-xl"
+                  label="Ending Time"
+                />
+              </td>
+            </tr>
+            <tr>
+              <td className="bg-[#404040] p-2 border border-[#5B5B5B]">
+                Thursday
+              </td>
+              <td className="bg-[#404040] p-2 border border-[#5B5B5B]">
+                <TimePicker
+                  className="bg-[#5B5B5B] rounded-xl"
+                  label="Starting Time"
+                />
+              </td>
+              <td className="bg-[#404040] p-2 border border-[#5B5B5B]">
+                <TimePicker
+                  className="bg-[#5B5B5B] rounded-xl"
+                  label="Ending Time"
+                />
+              </td>
+            </tr>
+            <tr>
+              <td className="bg-[#404040] p-2 border border-[#5B5B5B]">
+                Friday
+              </td>
+              <td className="bg-[#404040] p-2 border border-[#5B5B5B]">
+                <TimePicker
+                  className="bg-[#5B5B5B] rounded-xl"
+                  label="Starting Time"
+                />
+              </td>
+              <td className="bg-[#404040] p-2 border border-[#5B5B5B]">
+                <TimePicker
+                  className="bg-[#5B5B5B] rounded-xl"
+                  label="Ending Time"
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </LocalizationProvider>
     </div>
   );
 };
