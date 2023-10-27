a0e1973c117704af4c300ea1f64946e2b8fa4a91
EggsyOnCode <farzansaqib2003@gmail.com>
2023-10-27 12:01:11 +0500

    course edit page:scheduling: course completion duration complete

--- src/components/CourseEdit/Schedule.tsx
+++ src/components/CourseEdit/Schedule.tsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState } from "react";
+import { IoPush } from "react-icons/io5";
+import useCourseDifficulty from "../../hooks/courseDiffHook";
 
 const Schedule = () => {
+  const [setMonth, setMonthDuration] = useCourseDifficulty(0, 12);
+  const [setDays, setDaysDuration] = useCourseDifficulty(0, 30);
+
   return (
     <div className="w-full flex flex-col justify-start items-start text-text">
       <div className="p-12 font-bold">
@@ -8,9 +13,33 @@ const Schedule = () => {
         <hr className="h-[3px]" />
       </div>
 
-      
+      {/* Expected Completion Time */}
+      <h1 className="text-3xl mb-5 px-12 font-semibold">Learning Outcomes</h1>
+      <p className="mb-4 text-center text-xl px-12 font-normal">
+        Time interval in which the vocational training would be completed?
+      </p>
+      <div className="flex flex-row w-fit justify-evenly px-12 items-center">
+        <input
+          placeholder="Months (0 possible too)"
+          className="text-accent rounded-lg p-2 grow mr-4"
+          type="number"
+          value={setMonth}
+          onChange={(e) => setMonthDuration(e.target.value)}
+        />
+        <input
+          placeholder="Days"
+          className="text-accent rounded-lg p-2 mr-4 grow"
+          type="number"
+          value={setDays}
+          onChange={(e) => setDaysDuration(e.target.value)}
+        />
+
+        <div className="hover:cursor-pointer">
+          <IoPush size={24} />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default Schedule
+export default Schedule;
